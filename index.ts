b0bed377e1edd200a76d0c0760d233db16b2facb
Paweł Papkiewicz <papkie12@gmail.com>
2017-10-16 15:20:27 +0200

    Speedup

--- index.ts
+++ index.ts
@@ -1,10 +1,5 @@
 import * as getPixels from "get-pixels"
 
-function rgb2hex(red, green, blue) {
-    var rgb = blue | (green << 8) | (red << 16);
-    return (0x1000000 + rgb).toString(16).slice(1)
-}
-
 function getPixelArray(imagePath: string): Promise<any> {
     return new Promise((resolve, reject) => {
         getPixels(imagePath, (err, pixels) => {
@@ -19,33 +14,33 @@ function getPixelArray(imagePath: string): Promise<any> {
 async function findImage(image, source) {
     const imageToFind = await getPixelArray(image)
     const imageSource = await getPixelArray(source)
-    const [x, y] = imageToFind.shape
-    const [xSource, ySource] = imageSource.shape
-    if (xSource < x || ySource < y) {
+    const [sizeX, sizeY] = imageToFind.shape
+    const [sourceSizeX, ySource] = imageSource.shape
+    if (sourceSizeX < sizeX || ySource < sizeY) {
         throw new Error('Source image cannot be smaller')
     }
-    console.log([x,y],[xSource,ySource]);
+    console.log([sizeX,sizeY],[sourceSizeX,ySource]);
     const parsedToFindArray = []
     for(let iShape = 0; iShape<imageToFind.shape[0]; ++iShape) {
         for(let jShape=0; jShape<imageToFind.shape[1]; ++jShape) {
             const r = imageToFind.get(iShape, jShape, 0)
             const g = imageToFind.get(iShape, jShape, 1)
             const b = imageToFind.get(iShape, jShape, 2)
-            parsedToFindArray.push(rgb2hex(r,g,b))
+            parsedToFindArray.push([r,g,b])
         }
     }
 
     // console.log(cutImage);
     // console.log(imageSource.get(0,0,1))
 
-    const xMax = xSource - x
-    const yMax = ySource - y
+    const xMax = sourceSizeX - sizeX
+    const yMax = ySource - sizeY
     for (let i = 0; i < xMax; i++) {
         for (let j = 0; j < yMax; j++) {
-            if (j%30 === 0) {
+            if (j === 0) {
                 console.log(i,j);
             }
-            let cutImage = imageSource.hi(x+i,y+j).lo(i, j)
+            let cutImage = imageSource.hi(sizeX+i,sizeY+j).lo(i, j)
             let parsedArray = []
             let index = 0
             loop1:
@@ -54,10 +49,9 @@ async function findImage(image, source) {
                     const r = cutImage.get(iShape, jShape, 0)
                     const g = cutImage.get(iShape, jShape, 1)
                     const b = cutImage.get(iShape, jShape, 2)
-                    if (parsedToFindArray[index] !== rgb2hex(r,g,b)) {
+                    if (parsedToFindArray[index][0] !== r || parsedToFindArray[index][1] !== g || parsedToFindArray[index][2] !== b) {
                         break loop1;
                     }
-
                     index++;
                 }
             }
@@ -71,7 +65,9 @@ async function findImage(image, source) {
 }
 
 async function test() {
+    console.time('test')
     console.log(await findImage("assets/find.png", "assets/source.png"))
+    console.timeEnd('test')
 }
 
 test()
